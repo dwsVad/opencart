497822f71abd7c5a991dac4b92eb686d67805ada
Vadim Protcenko <vadim.protcenko@corp.hitwe.com>
2018-04-04 16:32:21 +0300

    fix

--- upload/system/library/template/twig.php
+++ upload/system/library/template/twig.php
@@ -8,34 +8,52 @@ final class Twig {
 		// include and register Twig auto-loader
 		include_once(DIR_SYSTEM . 'library/template/Twig/Autoloader.php');
 		
-		\Twig_Autoloader::register();
+		\Twig_Autoloader::register();	
 	}
 	
 	public function set($key, $value) {
 		$this->data[$key] = $value;
 	}
 	
-	public function render($template, $cache = false) {
-		// specify where to look for templates
-		$loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);
-
-		// initialize Twig environment
-		$config = array('autoescape' => false);
-
-		if ($cache) {
-			$config['cache'] = DIR_CACHE;
-		}
-
-		$this->twig = new \Twig_Environment($loader, $config);
+	public function render($template, $cache = false, $template_name) {
+		$file = DIR_TEMPLATE . $template . '.tpl';
 		
-		try {
-			// load template
-			$template = $this->twig->loadTemplate($template . '.twig');
+		if($template_name == 'themegloballite') {
+			$file = str_replace("default/","themegloballite/",$file);
+		}
+		
+		if (is_file($file)) {
 			
-			return $template->render($this->data);
-		} catch (Exception $e) {
-			trigger_error('Error: Could not load template ' . $template . '!');
-			exit();	
-		}	
+			extract($this->data);
+
+			ob_start();
+
+			require($file);
+
+			return ob_get_clean();
+		} else {
+			// specify where to look for templates
+			$loader = new \Twig_Loader_Filesystem(DIR_TEMPLATE);	
+			
+			// initialize Twig environment
+			$config = array('autoescape' => false);
+			
+			// initialize Twig environment
+			if ($cache) {
+				$config['cache'] = DIR_CACHE;
+			}
+			
+			$this->twig = new \Twig_Environment($loader, $config);
+			
+			try {
+				// load template
+				$template = $this->twig->loadTemplate($template . '.twig');
+				
+				return $template->render($this->data);
+			} catch (Exception $e) {
+				trigger_error('Error: Could not load template ' . $template . '!');
+				exit();	
+			}	
+		}
 	}	
 }
